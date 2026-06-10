7ce1a9e4d2c98b5d396b455fb22be60fc1bc8e66
Abdelrahman Salah <abdelrahman15salah@gmail.com>
2026-06-10 10:15:30 +0300

    updated port

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ import dbconnection from "./src/db/connection.js";
 import authRouter from "./src/modules/auth/auth.router.js";
 import dotenv from "dotenv";
 const app = express();
-
 app.use(express.json());
 
 dotenv.config();
+const port = process.env.PORT;
 dbconnection();
 
 app.use("/user", testRouter);
@@ -15,6 +15,6 @@ app.use("/auth", authRouter);
 app.use((req, res) => {
   res.status(404).json("404 Not Found");
 });
-app.listen(3000, () => {
+app.listen(port, () => {
   console.log("Server is running on port 3000");
 });
